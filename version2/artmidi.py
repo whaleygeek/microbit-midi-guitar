d4d886bc14e30b972580776d752ffb62daa13c5e
David Whale <david@thinkingbinaries.com>
2018-11-05 23:54:10 +0000

    Dynamic notation ruler added, all stored notes and intervals removed. Can now play a (hard coded) major chord at any position on any string.

--- version2/artmidi.py
+++ version2/artmidi.py
@@ -1,20 +1,33 @@
 from microbit import *
 from micropython import kbd_intr
 
-octave=0
+octaveidx=0
 OCTAVES = [3*12,4*12,5*12]
 
-rootidx = 0
-ROOTT = "EADGB"
-ROOTS = [4,9,2,7,11]
+stringidx = 0
+STRINGNAMES = "EADGB"
+OPENSTRINGS = [4,9,2,7,11] # C+semitones
+rootidx = OPENSTRINGS[stringidx]
+
 #   C C# D D# E F F# G G# A A# B
 #   0 1  2 3  4 5 6  7 8  9 10 11
+def getsemi(sp):
+    return OCTAVES[octaveidx] + rootidx + sp
 
+#       (0)    R 2 3 4 5 6 7  8  9  10 11 12 13 14 15
+TETRA = (None, 0,2,4,5,7,9,11,12,14,16,17,19,21,23,24)
+
+def getnote(np):
+    return getsemi(TETRA[np])
 
 DPINS = (pin8,pin12,pin13,pin14,pin15,pin16)
 PINS = (pin1,pin8,pin12,pin2,pin13,pin14,pin15,pin16)
 SCAN = None
 MASKS = (1,2,4,8,16,32,64,128)
+PLUCK=1<<5
+LOW=1<<4
+MID=1<<3
+HIGH=1<<2
 
 def init():
     kbd_intr(-1)
@@ -73,64 +86,32 @@ class MIDI():
             for n in notes:
                 self.send(self.NOTE_OFF | self.c, n, velocity)
 
-#TODO: deprecate, and use an on the fly 'notation ruler' for better range!
-def newnotes():
-    global NOTES
-    NOTES = [n+ROOTS[rootidx]+OCTAVES[octave] for n in range(9)]
-
-def reroot(i=None):
-    global rootidx, NOTES
-    if i is None:
-        rootidx = (rootidx+1) % len(ROOTS)
-    else:
-        rootidx = i
-    newnotes()
-
-def reoctave(i=None):
-    global octave
-    if i is None:
-        octave = (octave+1) % len(OCTAVES)
-    else:
-        octave = i
-    newnotes()
-
 midi = MIDI()
 
 playing = None
 plucked = False
-PLUCK = 1<<5
-LOW=1<<4
-MID=1<<3
-HIGH=1<<2
-
-newnotes()
 
 def send(tm, am, chords=False):
     global playing, plucked
 
-    if not chords:
+    if not chords: #single note
         if tm == 0:
-            notes = NOTES[0]
+            notes = getsemi(0) # open string
         else:
-            for mi in range(7, -1, -1):
-                if tm & MASKS[mi]:
-                    tm = MASKS[mi] # highest set
-                    break
+            for ni in range(7, -1, -1):
+                if tm & MASKS[ni]:
+                    break # highest fingered fret is wanted note
 
-            notes = NOTES[1+MASKS.index(tm)]
+            notes = getsemi(1+ni) # fingered string
     else: #chord
         if tm == 0:
-            notes = []
+            notes = [] # silence
         else:
             for ri in range(8):
                 if tm & MASKS[ri]:
-                    break
+                    break # lowest fingered fret is root
             ri += 1 #skip open string pos
-            try:
-                notes = [NOTES[ri]] # root
-                notes.append(NOTES[ri+4]) #3rd
-                notes.append(NOTES[ri+7]) #5th
-            except: pass
+            notes = [ri+getnote(1), ri+getnote(3), ri+getnote(5)] # MAJ
 
     pluck = (am & PLUCK) != 0
     if pluck and not plucked: # just plucked
@@ -147,14 +128,16 @@ def send(tm, am, chords=False):
     plucked = pluck
 
 def play():
+    global octaveidx, stringidx, rootidx
     pam = 0
     ptm = 0
     chords = False
-    display.show(ROOTT[rootidx])
+    display.show(STRINGNAMES[stringidx])
     while True:
         if button_a.was_pressed():
-            reroot()
-            display.show(ROOTT[rootidx])
+            stringidx = (stringidx + 1) % len(OPENSTRINGS)
+            rootidx = OPENSTRINGS[stringidx]
+            display.show(STRINGNAMES[stringidx])
 
         if button_b.was_pressed():
             chords = not chords
@@ -168,11 +151,11 @@ def play():
         am = rdart()
 
         if (am & LOW) != 0:
-            reoctave(0)
+            octaveidx = 0
         elif (am & MID) != 0:
-            reoctave(1)
+            octaveidx = 1
         elif (am & HIGH) != 0:
-            reoctave(2)
+            octaveidx = 2
 
         if am != pam or tm != ptm:
             send(tm, am, chords)
